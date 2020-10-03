31e94aa22587d3ea9a8ebd93d9f3541ec31cf321
Juhana Kuparinen <jmkupa@utu.fi>
2020-10-03 23:07:35 +0300

    Home sivu sommiteltu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Home.css";
 
 function Home() {
-	return (
-		<div>
-			<h1>Cards Against Humanists</h1>
-			<Link to="/new">New Game</Link>
-			<Link to="/join">Join Game</Link>
-		</div>
-	);
+  return (
+    <div id="Home">
+      <h1>Cards Against Humanists</h1>
+      <Link className="link" to="/new">
+        Uusi peli
+      </Link>
+      <Link className="link" to="/join">
+        Liity peliin
+      </Link>
+    </div>
+  );
 }
 
 export default Home;
