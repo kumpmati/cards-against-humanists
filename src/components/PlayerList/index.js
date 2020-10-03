0661f3fe51d50fc995c448315929b70f76995cf5
Matias Kumpulainen <matias.v.kumpulainen@utu.fi>
2020-10-04 00:26:46 +0300

    revamped room layout

--- src/components/PlayerList/index.js
+++ src/components/PlayerList/index.js
@@ -1,29 +1,33 @@
-import React, { useState } from "react";
-import PlayerListStyle from "./PlayerList.css";
+import React from "react";
+import "./PlayerList.css";
 
-import List from "../List";
 import Player from "../Player";
-
 import stateMessages from "./stateMessages";
 
 /*
  * Player list
  */
 function PlayerList({ players, current_czar, game_status, host, userId }) {
-	const currentMessage = stateMessages[game_status] || (() => null);
+  const currentMessage = stateMessages[game_status] || (() => null);
 
-	return (
-		<section id="players">
-			<div id="nav-info">
-				<div id="toggle-players">
-				</div>
-				<h1>{currentMessage(userId === current_czar)}</h1>
-			</div>
-			<div>
-				{players ? players.map(p => <Player data={p} isHost={host === p.id} isCzar={current_czar === p.id} />) : null}
-			</div>
-		</section>
-	);
+  return (
+    <section id="players">
+      <div id="nav-info">
+        <h1>{currentMessage(userId === current_czar)}</h1>
+      </div>
+      <div id="player-list">
+        {players
+          ? players.map(p => (
+              <Player
+                data={p}
+                isHost={host === p.id}
+                isCzar={current_czar === p.id}
+              />
+            ))
+          : null}
+      </div>
+    </section>
+  );
 }
 
 export default PlayerList;
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -12,24 +12,24 @@ import List from "../List";
  * Table
  */
 function Table({ current_question, submitted_cards, game_status, send }) {
-	const spreadCards = game_status === "CZAR_CHOOSES_ANSWER";
+  const spreadCards = game_status === "CZAR_CHOOSES_ANSWER";
 
-	return (
-		<section id="table">
-			<div id="current-question">
-				{current_question && <Card data={current_question} />}
-			</div>
+  return (
+    <section id="table">
+      <div id="current-question">
+        {current_question && <Card data={current_question} />}
+      </div>
 
-			<List
-				id="submitted-cards"
-				className={spreadCards ? "spread" : ""}
-				items={submitted_cards}
-				Component={CardPack}
-				// submit the clicked item as the winner
-				itemOnClick={(item) => send({ winner: item })}
-			/>
-		</section>
-	);
+      <List
+        id="submitted-cards"
+        className={spreadCards ? "spread" : ""}
+        items={submitted_cards}
+        Component={CardPack}
+        // submit the clicked item as the winner
+        itemOnClick={item => send({ winner: item })}
+      />
+    </section>
+  );
 }
 
 export default Table;
