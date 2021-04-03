45e8dfc6619f84d64bafd1c1dea8e7f953b59eef
mvkump <mvkump@utu.fi>
2021-04-03 22:06:22 +0300

    removed console log

--- templates/GamePage/Phases/Play/Play.tsx
+++ templates/GamePage/Phases/Play/Play.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./style.module.css";
 
 const Play = (game: any) => {
-  console.log(game);
   return (
     <div className={styles.container}>
       <h1>coming soon...</h1>
