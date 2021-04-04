c27df01d463307b599268afd31546363ed6b4b6a
mvkump <mvkump@utu.fi>
2021-04-05 00:07:22 +0300

    Tweaked hand and table CSS and added card submitting

--- templates/GamePage/Phases/Play/Hand/Hand.tsx
+++ templates/GamePage/Phases/Play/Hand/Hand.tsx
@@ -1,16 +1,22 @@
 import React, { useContext } from "react";
 import Card from "../../../../../components/Card/Card";
 import { AnswerCard } from "../../../../../game/types";
-
 import { PlayContext } from "../Play";
+
+import styles from "./style.module.css";
 
 const Hand = () => {
   const { game } = useContext(PlayContext);
   const { hand } = game.G;
+
+  const submitCard = (card: AnswerCard) => {
+    game.moves.submitAnswer([card]);
+  };
+
   return (
-    <div>
+    <div className={styles.hand}>
       {hand.map((card: AnswerCard) => (
-        <Card key={card.text} card={card} onClick={() => console.log(card)} />
+        <Card key={card.text} card={card} onClick={() => submitCard(card)} />
       ))}
     </div>
   );
--- templates/GamePage/Phases/Play/Table/Table.tsx
+++ templates/GamePage/Phases/Play/Table/Table.tsx
@@ -7,14 +7,34 @@ import styles from "./style.module.css";
 const Table = () => {
   const { isCzar, G } = useContext(PlayContext);
 
-  const question = G.table.question;
+  const { question, answers } = G.table;
+  const hasAnswers = answers.length > 0;
+
+  const tableClassNames = `${styles.table} ${
+    hasAnswers ? styles["table--answers"] : ""
+  }`;
+
+  const questionClassNames = `${styles.question} ${
+    hasAnswers ? styles["question--small"] : ""
+  }`;
 
   return (
-    <div className={styles.table}>
+    <div className={tableClassNames}>
       {isCzar && <h1>You are the Czar</h1>}
-      <div className={styles.question}>
+      <div className={questionClassNames}>
         {question && <Card card={question} />}
       </div>
+      <ul className={styles.answers}>
+        {answers.map((answer, i) => {
+          return (
+            <li key={i}>
+              {answer.map((card) => (
+                <Card key={card.text} card={card} />
+              ))}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
