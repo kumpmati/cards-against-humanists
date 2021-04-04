9123e4c9d67e0fadf66d90aabf5df51a969cc177
mvkump <mvkump@utu.fi>
2021-04-04 17:49:00 +0300

    Converted play stages to enum

--- game/phases/play.ts
+++ game/phases/play.ts
@@ -1,32 +1,41 @@
 import { PhaseConfig } from "boardgame.io";
-import { pickWinnerCard } from "../moves";
 
-// Winner choosing phase
+export enum PlayStages {
+  chooseCard = "chooseCard",
+  pickWinner = "pickWinner",
+  waitForOthers = "waitForOthers",
+  waitForCzar = "waitForCzar",
+}
+
+/**
+ * Phase where the actual gameplay happens
+ */
 const play: PhaseConfig = {
-  moves: {
-    pickWinnerCard,
-  },
   turn: {
     stages: {
-      // stage where players other than the Czar choose their cards
-      chooseCard: {
+      // players other than the Czar submit their answers
+      [PlayStages.chooseCard]: {
         moves: {
-          chooseCard: () => {},
+          submitAnswer: () => {},
         },
       },
 
-      // stage where Czar picks the winner
-      pickWinner: {
+      // Czar picks the winner
+      [PlayStages.pickWinner]: {
         moves: {
-          pickWinner: () => {},
+          chooseWinner: () => {},
         },
       },
 
-      // stage where the Czar waits for the other players to choose their cards
-      waitForOthers: {},
+      // Czar waits for the other players to choose their cards
+      [PlayStages.waitForOthers]: {
+        moves: {},
+      },
 
-      // stage where the other players wait for the czar to pick the winner
-      waitForCzar: {},
+      // players wait for the Czar to pick the winner
+      [PlayStages.waitForCzar]: {
+        moves: {},
+      },
     },
   },
 };
