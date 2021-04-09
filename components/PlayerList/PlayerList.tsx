5f96d1baa7c1cc9577b107243f76fe57fd81ac4c
mvkump <mvkump@utu.fi>
2021-04-09 13:36:16 +0300

    stuff 2

--- components/PlayerList/PlayerList.tsx
+++ components/PlayerList/PlayerList.tsx
@@ -7,8 +7,8 @@ const PlayerList: FC<Props> = ({ items = [], noTitle }) => {
       {!noTitle && <h2>Players:</h2>}
       <ul className={styles.list}>
         {items
-          ?.filter(i => i.isConnected)
-          .map(item => (
+          ?.filter((i) => i.isConnected)
+          .map((item) => (
             <li className={styles.list__item} key={item.id}>
               {item.name}
             </li>
