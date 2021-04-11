169b68b917fb9b64626ed09aac5a5448921a0afb
mvkump <mvkump@utu.fi>
2021-04-11 20:41:47 +0300

    Added console.log github handle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import Head from "next/head";
+import { useEffect } from "react";
 import "../styles/globals.css";
+import { printCredits } from "../util";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(printCredits, []);
+
   return (
     <>
       <Head>
--- util/index.ts
+++ util/index.ts
@@ -1,2 +1,9 @@
 export const sleep = async (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
+
+export const printCredits = () => {
+  console.log(
+    "%c@kumpmati on GitHub",
+    "background-color: black; color: white; padding: .6em 1.25em; margin: .25em; font-size: 1.25em"
+  );
+};
