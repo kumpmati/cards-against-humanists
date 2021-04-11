8398c0797d051664825ff338b96339f07686f0e4
mvkump <mvkump@utu.fi>
2021-04-11 12:58:19 +0300

    Edited info and added library credits

--- templates/InfoPage/index.tsx
+++ templates/InfoPage/index.tsx
@@ -29,29 +29,29 @@ const InfoPage = () => {
             <p>
               The game is played in rounds, and each round one player is the{" "}
               <b>Czar</b>. At the beginning of the round, the Czar gets a
-              question card, and reads it out loud to the other players. All the
-              other players must now submit an answer card, one which they think
-              will win. The cards will be face down until everyone has submitted
+              question card, and reads it to the other players. All the other
+              players must then submit one or more cards which they think are
+              funny. The cards will be face down until everyone has submitted
               their cards.
             </p>
             <p>
-              After every player has submitted an answer (or the round timer has
-              run out), the cards are shown. The Czar will then read the
-              question out loud again, substituting the blank space(s) in the
-              question card with the given answer cards. The Czar must then pick
-              the winning card(s). The player who submitted the winning card(s)
-              gets a point, and then a new round begins.
+              After every player has submitted an answer, the cards are shown.
+              The Czar will then read the question out loud again, substituting
+              the blank space(s) in the question card with the given answer
+              cards. The Czar must then pick the winning card(s). The player who
+              submitted the winning card(s) gets a point and is chosen as the
+              next Czar. A new round then begins.
             </p>
           </section>
           <section>
             <h2>Website</h2>
             <p>
-              This is a passion project by Matias Kumpulainen. Here's his{" "}
+              This is a project by Matias Kumpulainen. Here's his{" "}
               <a href="https://mvkump.dev">Website</a>.
             </p>
             <p>
               This project is open source. If you want to see the code, here are
-              the links:
+              the GitHub links:
             </p>
             <ul>
               <li>
@@ -62,6 +62,43 @@ const InfoPage = () => {
               <li>
                 <a href="https://github.com/kumpmati/cards-against-humanists-api">
                   Back-end
+                </a>
+              </li>
+            </ul>
+          </section>
+          <section>
+            <h3>Frameworks and libraries</h3>
+            <ul>
+              <li>
+                <a
+                  href="https://nextjs.org/"
+                  target="noreferrer"
+                  referrerPolicy="no-referrer">
+                  Next.js
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://boardgame.io"
+                  target="noreferrer"
+                  referrerPolicy="no-referrer">
+                  Boardgame.io
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://firebase.google.com/"
+                  target="noreferrer"
+                  referrerPolicy="no-referrer">
+                  Firebase
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://feathericons.com/"
+                  target="noreferrer"
+                  referrerPolicy="no-referrer">
+                  Feather icons
                 </a>
               </li>
             </ul>
