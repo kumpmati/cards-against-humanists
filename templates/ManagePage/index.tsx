13901961310f4934d2d6aba4f017664b240f62eb
mvkump <mvkump@utu.fi>
2021-04-11 15:15:53 +0300

    Added manage page (no content yet)

---
+++ templates/ManagePage/index.tsx
@@ -0,0 +1,32 @@
+import Head from "next/head";
+import React from "react";
+import { ArrowLeft } from "react-feather";
+import Button from "../../components/Button";
+import styles from "./style.module.css";
+
+const ManagePage = () => {
+  return (
+    <main className={styles.container}>
+      <Head>
+        <title>Cards Against Humanists | Manage</title>
+      </Head>
+
+      <nav>
+        <Button href="/" text="Back" Icon={ArrowLeft} />
+      </nav>
+
+      <div className="content">
+        <div id={styles.header}>
+          <h1 className="title">Manage</h1>
+          <p>Add new cards and stuff</p>
+        </div>
+
+        <section>
+          <h2>Cards</h2>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default ManagePage;
