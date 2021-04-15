87c35285bbfeceb89b7104b67d88b46c02a62348
mvkump <mvkump@utu.fi>
2021-04-15 15:55:53 +0300

    simplified static props

--- pages/manage/index.tsx
+++ pages/manage/index.tsx
@@ -1,22 +1,3 @@
-import axios from "axios";
-import { API_URL } from "../../api/constants";
-import { CardPack } from "../../game/types";
 import ManagePage from "../../templates/ManagePage";
 
 export default ManagePage;
-
-export async function getStaticProps() {
-  const disabledPacks = ["CAH_en"]; // do not allow adding cards to these packs
-
-  const res = (await axios.get(`${API_URL}/packs`)).data;
-
-  const cardPacks = res.filter(
-    (pack: CardPack) => !disabledPacks.includes(pack.code)
-  );
-
-  return {
-    props: {
-      cardPacks,
-    },
-  };
-}
